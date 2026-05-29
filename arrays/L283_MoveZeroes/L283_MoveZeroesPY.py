71306434fd4aa1d227c40b13b6eb38da842b5014
Shivansh Gupta <shivanshgupta2505@gmail.com>
2026-05-29 19:44:57 +0530

    Solved L283 Move Zeroes Problem

---
+++ arrays/L283_MoveZeroes/L283_MoveZeroesPY.py
@@ -0,0 +1,10 @@
+def moveZeroes(nums):
+    j = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[j],nums[i] = nums[i],nums[j]
+            j += 1
+    return nums
+
+nums = [0,1,0,3,12]
+print(moveZeroes(nums))
