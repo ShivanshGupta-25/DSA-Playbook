81da97568790adca14616f67fb3d986f69f72e83
Shivansh Gupta <shivanshgupta2505@gmail.com>
2026-06-27 22:16:55 +0530

    Solved L14 Longest Common Prefix problem on leetcode

---
+++ string/L14_LongestCommonPrefix/L14_LongestCommonPrefixCPP.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& v) {
+        string ans="";
+        sort(v.begin(),v.end());
+        int n=v.size();
+        string first=v[0],last=v[n-1];
+        for(int i=0;i<min(first.size(),last.size());i++){
+            if(first[i]!=last[i]){
+                return ans;
+            }
+            ans+=first[i];
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<string> v={"flower","flow","flight"};
+    cout << obj.longestCommonPrefix(v);
+    return 0;
+}
