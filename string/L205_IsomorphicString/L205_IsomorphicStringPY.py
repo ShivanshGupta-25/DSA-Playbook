88c8a1301d80f59a04ffaacb9964aee13dd652c9
Shivansh Gupta <shivanshgupta2505@gmail.com>
2026-06-27 22:30:08 +0530

    Solved L205 Iosmorphic string problem on leetcode

---
+++ string/L205_IsomorphicString/L205_IsomorphicStringPY.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        indexS = [0] * 200
+        indexT = [0] * 200
+        
+        length = len(s)
+        
+        if length != len(t):
+            return False
+        
+        for i in range(length):
+            if indexS[ord(s[i])] != indexT[ord(t[i])]:
+                return False
+            
+            indexS[ord(s[i])] = i + 1
+            indexT[ord(t[i])] = i + 1
+        
+        return True
+    
+a = Solution()
+print(a.isIsomorphic("egg", "add")) 
