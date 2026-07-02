c4bd6e1005b6a13f5c8a8e4c363f8e329d614093
Your Name <shivanshgupta2505@gmail.com>
2026-07-02 23:14:04 +0530

    solved L451 Sort characters by frequency problem on leetcode

---
+++ string/L451_SortCharByFrequency/L451_SortCharByFrequencyPY.py
@@ -0,0 +1,18 @@
+import heapq
+from collections import Counter
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        counter = Counter(s)
+        pq = [(-freq, char) for char, freq in counter.items()]
+        heapq.heapify(pq)
+        result = ''
+        while pq:
+            freq, char = heapq.heappop(pq)
+            result += char * -freq
+        return result
+
+a = Solution()
+print(a.frequencySort("tree"))
+print(a.frequencySort("cccaaa"))
+print(a.frequencySort("Aabb"))
