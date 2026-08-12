1d72a9762d290b1694079004a063bcbbbfe527b1
Your Name <shivanshgupta2505@gmail.com>
2026-08-12 22:48:12 +0530

    Solved Leetcode problem L20 Valid Patentheses

---
+++ StackQueue/L20_ValidPatentheses/L20_ValidPatenthesesCPP.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char ch : s) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (st.empty()) {
+                    return false;
+                }
+                char top = st.top();
+                st.pop();
+                if (ch == ')' && top != '(') return false;
+                if (ch == ']' && top != '[') return false;
+                if (ch == '}' && top != '{') return false;
+            }
+        }
+        return st.empty();
+    }
+};
+
+int main()
+{
+    Solution obj;
+    cout << obj.isValid("()[]{}") << endl;
+    cout << obj.isValid("(]") << endl;
+    cout << obj.isValid("([)]") << endl;
+    
+    return 0;    
+}
